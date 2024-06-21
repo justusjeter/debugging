59be1f96c02597645802bb117e05585b77376633
Justus <justus@CISCAPEAR01sMBP.localdomain>
2024-06-21 10:57:34 -0400

    debugging

---
+++ debugging.js
@@ -0,0 +1,50 @@
+const storeOwners = [
+    {
+      name: 'Christian',
+      stores: 1,
+      location: "AZ",
+    },
+    {
+      name: 'Easton',
+      stores: 1,
+      location: "NM",
+    },
+    {
+      name: 'Daylen',
+      stores: 1,
+      location: "NM",
+    },
+    {
+      name: 'Justus',
+      stores: 1,
+      location: "NM",
+    },
+  ];
+  
+  const listNumberOfStores = function () {
+    for (let i = 0; i < storeOwners.length; i++) {
+      let totalLocations = totalLocaions + storeOwners.stores;
+    }
+    return i;
+  };
+  
+  let locations = listNumberOfStores;
+  
+  function tellMeMyStores() {
+    console.log('Hey, can you tell me how many stores you have?');
+    if (locations > 0) {
+      console.log('Of course, we have ' + locations + ' stores');
+    }
+  }
+  
+  function hasStore() {
+    for (let i = 0; i < storeOwners.length; i++) {
+      let person = storeOwners[i].name;
+      let location = storeOwners[i].location;
+      console.log(`Yes, ${person} has one in ${location}`);
+    }
+  
+  }
+  
+  tellMeMyStores();
+  hasStore();
